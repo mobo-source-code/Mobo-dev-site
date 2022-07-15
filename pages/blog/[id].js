664d8e6009c54ea238c587ff85969224a64689c4
Mohammed Bouaziz <mobo.repos@gmail.com>
2022-07-15 19:03:01 +0100

    ddqs

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -8,7 +8,7 @@ export const getStaticPaths = async () => {
     const res = await axios.get("https://mobodev.herokuapp.com/api/posts?populate=*")
     const posts = res.data.data
 
-    const paths = posts.map(b => {
+    const paths = posts?.map(b => {
         return {
             params: { id: b.id.toString()}
         }
@@ -34,17 +34,23 @@ export const getStaticProps = async (context) => {
 
 const Single = ({post}) => {
 
-    const da = new Date(post.attributes.publishedAt).toLocaleDateString()
+    const da = new Date(post?.attributes.publishedAt).toLocaleDateString()
 
     const md = new MarkdownIt();
-    const htmlContent = md.render(post.attributes.body)
+    let content = post?.attributes.body
+    let htmlContent = "# none"
+    if (typeof content === 'string' || content instanceof String) {
+        htmlContent = md.render(content)        
+    } else {
+        htmlContent = md.render("# hello")
+    }
 
     const SEO = {
-        title: post.attributes.title,
-        description: post.attributes.description,
+        title: post?.attributes.title,
+        description: post?.attributes.description,
         openGraph: {
-            title: post.attributes.title,
-            description: post.attributes.title
+            title: post?.attributes.title,
+            description: post?.attributes.title
         }
     }
 
@@ -53,12 +59,12 @@ const Single = ({post}) => {
             <NextSeo {...SEO} />
             <Nav />
             <div className="flex flex-col justify-center items-center mt-20">
-                <Image height={500} width={750} src={"https://mobodev.herokuapp.com" + post.attributes.cover.data.attributes.url} />
+                <Image height={500} width={750} src={"https://mobodev.herokuapp.com" + post?.attributes.cover.data.attributes.url} />
                 <h1 className="text-4xl font-bold text-center mt-28">
-                    {post.attributes.title}
+                    {post?.attributes.title}
                 </h1>
                 <p className="text-xl font-text mt-10 text-center text-gray-600 font-regular">
-                    {post.attributes.description}
+                    {post?.attributes.description}
                 </p>
                 <div className="text-md text-gray-500 mt-10 flex space-x-20 items-center">
                     <div className="flex items-center space-x-4">
