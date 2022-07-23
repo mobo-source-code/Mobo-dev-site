12c004f38a5ac5aa67b060032cfc7415ba35397e
Mohammed Bouaziz <mobo.repos@gmail.com>
2022-07-23 13:48:28 +0100

    analytics and sumo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 import nextSeoConfig from '../next-seo.config'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script strategy='lazyOnload' src='https://www.googletagmanager.com/gtag/js?id=G-J927T7L7XM'/>
+      <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-J927T7L7XM', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+      <Script strategy="afterInteractive" 
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  (function(s,u,m,o,j,v){j=u.createElement(m);v=u.getElementsByTagName(m)[0];j.async=1;j.src=o;j.dataset.sumoSiteId='7a20462fb80eeaa2b49821319a3082ca8cc5bc55782d31040aa253c39d94b9a3';v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/')
+                `,
+                }}
+                />
       <DefaultSeo {...nextSeoConfig} />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -40,13 +40,6 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&family=Ubuntu:wght@400;500;700&family=Varela+Round&display=swap"
           rel="stylesheet"
         />
-        <Script strategy="afterInteractive" 
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  (function(s,u,m,o,j,v){j=u.createElement(m);v=u.getElementsByTagName(m)[0];j.async=1;j.src=o;j.dataset.sumoSiteId='7a20462fb80eeaa2b49821319a3082ca8cc5bc55782d31040aa253c39d94b9a3';v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/')
-                `,
-                }}
-                />
       </Head>
       <header>
         <Nav />
